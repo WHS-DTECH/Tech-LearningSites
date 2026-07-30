ed4aba00bf1e596eebdb04ff235d39c90f49783f
WHS-DTECH <vanessapringle@westlandhigh.school.nz>
2026-07-31 11:22:46 +1200

    Serve PDF files with inline content type

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const contentTypes = {
   ".ico": "image/x-icon",
   ".js": "application/javascript; charset=utf-8",
   ".json": "application/json; charset=utf-8",
+  ".pdf": "application/pdf",
   ".png": "image/png",
   ".svg": "image/svg+xml",
   ".txt": "text/plain; charset=utf-8",
