d4906e0f4be1ce5b5b05a3de965edbdf99747008
WHS-DTECH <vanessapringle@westlandhigh.school.nz>
2026-09-14 16:09:19 +1200

    Add admin portal source and update note

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("**/*.pdf");
+  eleventyConfig.addGlobalData("siteContentLastUpdated", "14 September 2026");
 
   return {
     dir: {
